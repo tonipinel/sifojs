c059d3f3283f9233b7567101666f972069aaa486
Toni <tonipinel@gmail.com>
2013-03-25 11:37:11 +0100

    Commit SIFO JS to github

---
+++ core/namespace.js
@@ -0,0 +1,23 @@
+var CORE = {
+	utilities : {},
+	globals : {}, 
+	instance : {},
+	modules : [],
+	classes : {},
+	behaviour : {
+		page : {},
+		modules : {},
+		nullFunction : function(){} 
+	}
+};
+var SITE = {
+	utilities : {},
+	globals : {},
+	instance : {},
+	modules : {},
+	classes : {},
+	behaviour : {
+		page : {},
+		nullFunction : function(){}
+	}
+};
