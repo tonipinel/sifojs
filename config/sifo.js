6cf733e3f659d97da4b3b312354febd431956a09
Toni <tonipinel@gmail.com>
2013-03-27 18:12:22 +0100

    config para juicer

---
+++ config/sifo.js
@@ -0,0 +1,6 @@
+// @depend ../libs/labjs/LAB.js
+// @depend ../core/ready.js
+// @depend ../core/device_detection.js
+// @depend ../core/namespace.js
+// @depend ../core/utilities.js
+// @depend ../core/init.js
